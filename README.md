65791f47df8c9897434b40d186162229f3538dcf
白浪 <ahbailang@gmail.com>
2020-01-10 17:08:59 +0800

    增加openjdk - fontconfig 的解决方案

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docker
 docker shell
+
+- openjdk  
+增加fontconfig 的解决方案
+RUN apk add --update ttf-dejavu && rm -rf /var/cache/apk/*
+RUN fc-cache --force
---
+++ ci.sh
@@ -0,0 +1,3 @@
+# 在相应目录下执行
+docker build -t registry.cn-shanghai.aliyuncs.com/ahbxl/openjdk:8-jdk-alpine .
+docker push registry.cn-shanghai.aliyuncs.com/ahbxl/openjdk:8-jdk-alpine
